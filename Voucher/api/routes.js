dad230b237e69e4c71667c7d18cc56ef7641eb6a
Le Minh Hung <hung.leminh@hotmail.com>
2022-01-13 16:17:52 +0700

    [HungLM] add code export api

--- Voucher/api/routes.js
+++ Voucher/api/routes.js
@@ -1,24 +1,39 @@
 'use strict';
 module.exports = function(app) {
-    let voucherController = require('./controller/VoucherController');
-    let eventController = require('./controller/EventController');
-    // api get a voucher
-    app.route('/voucher/generate')
-      .get(voucherController.generateVoucher);
+  /* user controller */
+  let userController = require('./controller/UserController');
 
-    // api get a voucher
-    app.route('/events/generate')
-      .post(eventController.generateEvent);
+  /* voucher controller */
+  let voucherController = require('./controller/VoucherController');
 
-    // api get a voucher
-    app.route('/events/:event_id/editable/me')
-      .post(eventController.editEvent);
+  /* event controller */
+  let eventController = require('./controller/EventController');
 
-    // api get a voucher
-    app.route('/events/:event_id/editable/release')
-      .post(eventController.releaseEvent);
+  // api get a voucher
+  app.route('/user/signup')
+    .post(userController.signUp);
 
-    // api get a voucher
-    app.route('/events/:event_id/editable/maintain')
-      .post(eventController.releaseEvent);
+  // api get a voucher
+  app.route('/user/signin')
+    .post(userController.signIn);
+  
+  // api get a voucher
+  app.route('/voucher/generate')
+    .get(voucherController.generateVoucher);
+
+  // api get a voucher
+  app.route('/events/generate')
+    .post(eventController.generateEvent);
+
+  // api get a voucher
+  app.route('/events/:event_id/editable/me')
+    .post(eventController.editEvent);
+
+  // api get a voucher
+  app.route('/events/:event_id/editable/release')
+    .post(eventController.releaseEvent);
+
+  // api get a voucher
+  app.route('/events/:event_id/editable/maintain')
+    .post(eventController.releaseEvent);
 };
